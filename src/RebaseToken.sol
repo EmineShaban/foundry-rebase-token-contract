bc24aa16fab9cd49cf17fd32970a1f0c461532c2
Emine Shaban <emineshaban@icloud.com>
2025-02-27 12:44:21 +0200

    Finish Rebase-token-contract

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,48 +1,90 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract RebaseToken is ERC20{
+
+contract RebaseToken is ERC20 {
     error RebaseToken__InterestReteCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
     uint256 private constant PRECISION_FACTOR = 1e18;
     uint256 private s_interestRate = 5e10;
     mapping(address => uint256) private s_userInterestRate;
-        mapping(address => uint256) private s_userLastUpdatedTimestamp;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
-        event InterestRateSet(uint256 newInterestRate);
-        constructor() ERC20("Rabase Token", "RBT") {
- 
+    event InterestRateSet(uint256 newInterestRate);
+
+    constructor() ERC20("Rabase Token", "RBT") {}
+
+    function setInterestRate(uint256 _newInterestRate) external {
+        if (_newInterestRate < s_interestRate) {
+            revert RebaseToken__InterestReteCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
+        s_interestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
+    }
 
-        function setInterestRate(uint256 _newInterestRate) external {
-            if(_newInterestRate < s_interestRate){
-                revert RebaseToken__InterestReteCanOnlyDecrease(s_interestRate, _newInterestRate);
-            }
-            s_interestRate = _newInterestRate;
-            emit InterestRateSet(_newInterestRate);
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruesInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_from);
         }
+        _mintAccruesInterest(_from);
+        _burn(_from, _amount);
+    }
 
-        function mint(address _to, uint256 _amount) external {
-            _mintAccruesInterest(_to);
-            s_userInterestRate[_to] = s_interestRate;
-            _mint(_to, _amount);
+    function balanceOf(address _user) public view override returns (uint256) {
+        return super.balanceOf(_user) + _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruesInterest(msg.sender);
+        _mintAccruesInterest(_recipient);
+
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
         }
-
-        function balanceOf(address _user) public view override returns (uint256) {
-            return super.balanceOf(_user) + _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
         }
+        return super.transfer(_recipient, _amount);
+    }
 
-        function _calculateUserAccumulatedInterestSinceLastUpdate(address _user) internal view returns (uint256 linearInterest) {
+        function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruesInterest(_sender);
+        _mintAccruesInterest(_recipient);
 
-            uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
-            linearInterest = (PRECISION_FACTOR * (s_userInterestRate[_user] * timeElapsed));
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
         }
-
-        function _mintAccruesInterest(address _user) internal {
-            s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
         }
+        return super.transferFrom(_sender, _recipient, _amount);
+    }
 
-        function getUserInterestRate(address _user) external view returns (uint256) {
-            return s_userInterestRate[_user];
-        }
+    function _calculateUserAccumulatedInterestSinceLastUpdate(address _user)
+        internal
+        view
+        returns (uint256 linearInterest)
+    {
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        linearInterest = (PRECISION_FACTOR * (s_userInterestRate[_user] * timeElapsed));
+    }
+
+    function _mintAccruesInterest(address _user) internal {
+        uint256 previousPrincipleBalance = super.balanceOf(_user);
+        uint256 currentBalance = balanceOf(_user);
+        uint256 balanceIncrease = currentBalance - previousPrincipleBalance;
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        _mint(_user, balanceIncrease);
+    }
+
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate[_user];
+    }
 }

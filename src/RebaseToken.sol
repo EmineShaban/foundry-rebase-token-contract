09518dd809e679b6820da710c770abd88ec42a64
Emine Shaban <emineshaban@icloud.com>
2025-03-01 17:44:58 +0200

    Vault And-natspec

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,20 +2,29 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RebaseToken is ERC20 {
+contract RebaseToken is ERC20, Ownable, AccessControl {
     error RebaseToken__InterestReteCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
     uint256 private constant PRECISION_FACTOR = 1e18;
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
     uint256 private s_interestRate = 5e10;
     mapping(address => uint256) private s_userInterestRate;
     mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
     event InterestRateSet(uint256 newInterestRate);
 
-    constructor() ERC20("Rabase Token", "RBT") {}
+    constructor() ERC20("Rabase Token", "RBT") Ownable(msg.sender) {
+        //   _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
-    function setInterestRate(uint256 _newInterestRate) external {
+    function grantBurnAndMintRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
+    }
+
+    function setInterestRate(uint256 _newInterestRate) external onlyOwner {
         if (_newInterestRate < s_interestRate) {
             revert RebaseToken__InterestReteCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
@@ -23,13 +32,17 @@ contract RebaseToken is ERC20 {
         emit InterestRateSet(_newInterestRate);
     }
 
-    function mint(address _to, uint256 _amount) external {
+    function principleBalanceOf(address _user) external view returns (uint256) {
+        return super.balanceOf(_user);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
         _mintAccruesInterest(_to);
         s_userInterestRate[_to] = s_interestRate;
         _mint(_to, _amount);
     }
 
-    function burn(address _from, uint256 _amount) external {
+    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
         if (_amount == type(uint256).max) {
             _amount = balanceOf(_from);
         }
@@ -54,7 +67,7 @@ contract RebaseToken is ERC20 {
         return super.transfer(_recipient, _amount);
     }
 
-        function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
         _mintAccruesInterest(_sender);
         _mintAccruesInterest(_recipient);
 
---
+++ src/interfaces/IRebaseToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+interface IRebaseToken {
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _from, uint256 _amount) external;
+}
